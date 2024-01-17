835fc5d81b697787f34bc4dcb06bcd8254a3ab63
Mouri199 <138799900+Mouri199@users.noreply.github.com>
2024-01-17 15:52:51 +0600

    home page 70 parcent done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,10 @@ import Banner from "./Banner";
 import ChooseUs from "./ChooseUs";
 import DoctorSpecialist from "./DoctorSpecialist";
 import OurStory from "./OurStory";
+import Review from "./Review";
 import SpecialistCard from "./SpecialistCard";
+import img from '/last-section-bg.jpg'
+
 
 
 const Home = () => {
@@ -13,6 +16,17 @@ const Home = () => {
             <DoctorSpecialist></DoctorSpecialist>
             <ChooseUs></ChooseUs>
             <OurStory></OurStory>
+            <div className="hero h-[600px]" style={{ backgroundImage: `url(${img})` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div>
+                        <h1 className="mb-5 text-5xl font-bold">Buying medicines should be quick & easy!</h1>
+                        <p className="mb-5">Build an online pharmacy with IPharm</p>
+                        <button className="btn btn-primary">Nuy Medicine</button>
+                    </div>
+                </div>
+            </div>
+            <Review></Review>
         </div>
     );
 };
