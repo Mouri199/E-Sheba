079630a4114430e5d04fb581685dbca1451741f0
Mouri199 <138799900+Mouri199@users.noreply.github.com>
2024-01-19 11:49:33 +0600

    footer added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { MedicineData } from "../../assets/Data/Medicine";
 import Banner from "./Banner";
 import ChooseUs from "./ChooseUs";
 import DoctorSpecialist from "./DoctorSpecialist";
@@ -10,10 +11,20 @@ import img from '/last-section-bg.jpg'
 
 const Home = () => {
 
+    const uniqueCategories = Array.from(new Set(MedicineData.map(item => item.Category)));
+
     return (
         <div>
             <Banner></Banner>
             <SpecialistCard></SpecialistCard>
+            <div className="categories-list">
+                <h2>Categories:</h2>
+                <ul>
+                    {uniqueCategories.map(category => (
+                        <li key={category}>{category}</li>
+                    ))}
+                </ul>
+            </div>
             <DoctorSpecialist></DoctorSpecialist>
             <ChooseUs></ChooseUs>
             <OurStory></OurStory>
@@ -23,7 +34,7 @@ const Home = () => {
                     <div>
                         <h1 className="mb-5 lg:text-5xl text-3xl font-bold">Buying medicines should be quick & easy!</h1>
                         <p className="mb-5">Build an online pharmacy with IPharm</p>
-                        <button className="btn btn-primary">Buy Medicine</button>
+                        <button className="btn bg-[#0360D9] text-white">Buy Medicine</button>
                     </div>
                 </div>
             </div>
---
+++ src/assets/Data/Medicine.js
@@ -0,0 +1,302 @@
+export const MedicineData = [
+    {
+      "ID": 1,
+      "medName": "Aspirin",
+      "img": "https://i.ibb.co/YdrNNR2/i-Stock-458563393.jpg",
+      "Category": "Pain Relief",
+      "price": 5.99,
+      "ComName": "PharmaCorp",
+      "PublishDate": "2022-01-01",
+      "Des": "Effective for mild to moderate pain relief."
+    },
+    {
+      "ID": 2,
+      "medName": "Ibuprofen",
+      "img": "https://i.ibb.co/gDSp2Fg/GUEST-ed5349ff-fa70-4af9-ac28-d73d8dcbe9ef.jpg",
+      "Category": "Pain Relief",
+      "price": 7.49,
+      "ComName": "HeadEase",
+      "PublishDate": "2022-02-05",
+      "Des": "NonsteroIDal anti-inflammatory drug (NSAID) for pain and inflammation."
+    },
+    {
+      "ID": 3,
+      "medName": "Paracetamol",
+      "img": "https://i.ibb.co/4mgMBH3/3634764592.jpg",
+      "Category": "healthCare",
+      "price": 4.99,
+      "ComName": "HeadEase",
+      "PublishDate": "2022-03-10",
+      "Des": "Commonly used to reduce fever and relieve pain."
+    },
+    {
+      "ID": 4,
+      "medName": "Antihistamine",
+      "img": "https://i.ibb.co/Pxssyzy/generic-image-of-antihistamine-tablets-used-in-allergy-relief-chlorphenamine-BWH0-RJ.jpg",
+      "Category": "Allergy",
+      "price": 12.99,
+      "ComName": "PharmaCorp",
+      "PublishDate": "2022-04-15",
+      "Des": "ProvIDes relief from allergy symptoms such as sneezing and itching."
+    },
+    {
+      "ID": 5,
+      "medName": "Omega-3 Capsules",
+      "img": "https://i.ibb.co/pv2m3GB/Omaga-Mock-FInal-44.jpg",
+      "Category": "Supplements",
+      "price": 19.99,
+      "ComName": "HeadEase",
+      "PublishDate": "2022-05-20",
+      "Des": "Supports heart health and provIDes essential fatty acIDs."
+    },
+    {
+      "ID": 6,
+      "medName": "Vitamin C Tablets",
+      "img": "https://i.ibb.co/CvVF7Lj/VITAMIN-C.jpg",
+      "Category": "Supplements",
+      "price": 8.49,
+      "ComName": "HeadEase",
+      "PublishDate": "2022-06-25",
+      "Des": "Boosts immune system function and helps prevent illnesses."
+    },
+    {
+      "ID": 7,
+      "medName": "Allergy Nasal Spray",
+      "img": "https://i.ibb.co/QHnD6kJ/450.jpg",
+      "Category": "Allergy",
+      "price": 15.75,
+      "ComName": "HeadEase",
+      "PublishDate": "2022-07-10",
+      "Des": "Relieves nasal congestion and allergy symptoms."
+    },
+    {
+      "ID": 8,
+      "medName": "Muscle Relaxant",
+      "img": "https://i.ibb.co/fMBLMPY/en-mot-platinum-40s-ff-e-s-1.jpg",
+      "Category": "Pain Relief",
+      "price": 9.99,
+      "ComName": "PharmaCorp",
+      "PublishDate": "2022-08-15",
+      "Des": "Eases muscle tension and promotes relaxation."
+    },
+    {
+      "ID": 9,
+      "medName": "Acne Cream",
+      "img": "https://i.ibb.co/VMv38DR/download.jpg",
+      "Category": "Allergy",
+      "price": 14.50,
+      "ComName": "VitaJoy",
+      "PublishDate": "2022-09-20",
+      "Des": "Reduces acne and promotes clear skin."
+    },
+    {
+      "ID": 10,
+      "medName": "Dietary Fiber Supplement",
+      "img": "https://i.ibb.co/hMTRktr/71-Obj0-EFL-AC-UL600-SR600-600.jpg",
+      "Category": "Digestive",
+      "price": 11.25,
+      "ComName": "PharmaCorp",
+      "PublishDate": "2022-10-25",
+      "Des": "Supports digestive system health and regular bowel movements."
+    },
+    {
+      "ID": 11,
+      "medName": "Migraine Relief",
+      "img": "https://i.ibb.co/qmBds2c/migraine-relief-tablets-BHI-main-1.jpg",
+      "Category": "healthCare",
+      "price": 16.99,
+      "ComName": "HeartGuard",
+      "PublishDate": "2022-11-01",
+      "Des": "ProvIDes relief from migraine headaches."
+    },
+    {
+      "ID": 12,
+      "medName": "Calcium Tablets",
+      "img": "https://i.ibb.co/qmvvgY4/71q-C9-Q62-G3-L-AC-UF1000-1000-QL80.jpg",
+      "Category": "Digestive",
+      "price": 13.75,
+      "ComName": "PharmaCorp",
+      "PublishDate": "2022-12-06",
+      "Des": "Supports strong and healthy bones."
+    },
+    {
+      "ID": 13,
+      "medName": "Multivitamin Gummies",
+      "img": "https://i.ibb.co/bPVF5Yw/719-JK53-EMCL-AC-UF1000-1000-QL80.jpg",
+      "Category": "Digestive",
+      "price": 18.50,
+      "ComName": "Suppliment",
+      "PublishDate": "2023-01-10",
+      "Des": "Delicious gummies with essential vitamins and minerals."
+    },
+    {
+      "ID": 14,
+      "medName": "Anti-Anxiety Medication",
+      "img": "https://i.ibb.co/Jzb1Lx1/intro-1614804971.jpg",
+      "Category": "healthManagement",
+      "price": 22.99,
+      "ComName": "VitaJoy",
+      "PublishDate": "2023-02-15",
+      "Des": "Helps alleviate symptoms of anxiety and stress."
+    },
+    {
+      "ID": 15,
+      "medName": "Weight Loss Pills",
+      "img": "https://i.ibb.co/zJX95Kf/2a8145313abcf872f3f15328ac9d6d00.jpg",
+      "Category": "healthManagement",
+      "price": 29.99,
+      "ComName": "VitaJoy",
+      "PublishDate": "2023-03-22",
+      "Des": "Supports healthy weight loss and metabolism."
+    },
+    {
+      "ID": 16,
+      "medName": "Hair Growth Serum",
+      "img": "https://i.ibb.co/vv8BjH4/download-1.jpg",
+      "Category": "Skincare",
+      "price": 17.95,
+      "ComName": "HeartGuard",
+      "PublishDate": "2023-04-27",
+      "Des": "Promotes hair growth and reduces hair loss."
+    },
+    {
+      "ID": 17,
+      "medName": "Strength-20mg10s",
+      "img": "https://i.ibb.co/k0CnbZw/Stomach-Ache-Pain-Relief-Double-Strength-20mg10s-9350299002010-3-D-Perspective-Shifted-Top-Front-405.jpg",
+      "Category": "Pain Relief",
+      "price": 8.99,
+      "ComName": "HeartGuard",
+      "PublishDate": "2023-05-10",
+      "Des": "Relieves common stomach pain and discomfort."
+    },
+    {
+      "ID": 18,
+      "medName": "Opticrome",
+      "img": "https://i.ibb.co/n68g9xX/56558457-ec79-4c73-ad87-bf6f5e9d65a8-1966040934.jpg",
+      "Category": "Eye Care",
+      "price": 6.75,
+      "ComName": "AcIDRelief",
+      "PublishDate": "2023-06-15",
+      "Des": "Refreshes and soothes dry or irritated eyes."
+    },
+    {
+      "ID": 19,
+      "medName": "Joint Flex",
+      "img": "https://i.ibb.co/ncbKZf0/Joint-flex-114g-62497.jpg",
+      "Category": "Pain Relief",
+      "price": 14.99,
+      "ComName": "AcIDRelief",
+      "PublishDate": "2023-07-20",
+      "Des": "Eases joint pain and improves flexibility."
+    },
+    {
+      "ID": 20,
+      "medName": "Visine",
+      "img": "https://i.ibb.co/Qp58Mwq/00074300926483-vis-allergy-multi-single-obwb.jpg",
+      "Category": "Allergy",
+      "price": 10.50,
+      "ComName": "AcIDRelief",
+      "PublishDate": "2023-08-25",
+      "Des": "Relieves allergy symptoms affecting the eyes."
+    },
+    {
+      "ID": 21,
+      "medName": "Prenatal Vitamins",
+      "img": "https://i.ibb.co/MDhDPqg/wh-prenatalvitamins-1642545042.png",
+      "Category": "healthManagement",
+      "price": 24.99,
+      "ComName": "AcIDRelief",
+      "PublishDate": "2023-09-01",
+      "Des": "Supports the health of pregnant women and their developing babies."
+    },
+    {
+      "ID": 22,
+      "medName": "AntacID Tablets",
+      "img": "https://i.ibb.co/m9QXfch/dynacid-tablets.jpg",
+      "Category": "Digestive",
+      "price": 7.99,
+      "ComName": "AcIDRelief",
+      "PublishDate": "2023-10-06",
+      "Des": "Relieves heartburn and indigestion."
+    },
+    {
+      "ID": 23,
+      "medName": "Insect Repellent Spray",
+      "img": "https://i.ibb.co/sgsvCxq/fbd2140f8def236472c2b6b3aea38e83.jpg",
+      "Category": "Skincare",
+      "price": 11.99,
+      "ComName": "SunSafe",
+      "PublishDate": "2023-11-10",
+      "Des": "Repels mosquitoes and other insects for outdoor activities."
+    },
+    {
+      "ID": 24,
+      "medName": "Canies Pro",
+      "img": "https://i.ibb.co/7y4chp9/50-Image-5.jpg",
+      "Category": "Allergy",
+      "price": 13.25,
+      "ComName": "SunSafe",
+      "PublishDate": "2023-12-15",
+      "Des": "Treats and prevents fungal infections in nails."
+    },
+    {
+      "ID": 25,
+      "medName": "Sunscreen Lotion",
+      "img": "https://i.ibb.co/ScLykbq/2901200183.jpg",
+      "Category": "Skincare",
+      "price": 9.50,
+      "ComName": "SunSafe",
+      "PublishDate": "2024-01-20",
+      "Des": "Protects the skin from harmful UV rays."
+    },
+    {
+      "ID": 26,
+      "medName": "Colon Cleanser",
+      "img": "https://i.ibb.co/JxntRPK/1543-90-AFRONT.jpg",
+      "Category": "Skincare",
+      "price": 16.49,
+      "ComName": "SunSafe",
+      "PublishDate": "2024-02-25",
+      "Des": "Supports colon health and promotes regular bowel movements."
+    },
+    {
+      "ID": 27,
+      "medName": "Mouthwash",
+      "img": "https://i.ibb.co/Jzw5Nzm/lis-us-gum-therapy.jpg",
+      "Category": "healthCare",
+      "price": 5.75,
+      "ComName": "SunSafe",
+      "PublishDate": "2024-03-01",
+      "Des": "Freshens breath and kills germs for oral hygiene."
+    },
+    {
+      "ID": 28,
+      "medName": "Statin",
+      "img": "https://i.ibb.co/H2PbS3s/Statin-drugs.jpg",
+      "Category": "healthManagement",
+      "price": 23.99,
+      "ComName": "HeartGuard",
+      "PublishDate": "2024-04-05",
+      "Des": "Helps lower cholesterol levels for heart health."
+    },
+    {
+      "ID": 29,
+      "medName": "Sanitize-50ml-mockup",
+      "img": "https://i.ibb.co/Zd018DZ/Sanitize-50ml-mockup-500px.jpg",
+      "Category": "healthManagement",
+      "price": 6.99,
+      "ComName": "HeartGuard",
+      "PublishDate": "2024-05-10",
+      "Des": "Kills germs and bacteria for on-the-go hand hygiene."
+    },
+    {
+      "ID": 30,
+      "medName": "Alpha Brain",
+      "img": "https://i.ibb.co/rQRH1Rn/1696001903-81-Cade-AGlg-L.jpg",
+      "Category": "healthCare",
+      "price": 20.50,
+      "ComName": "HeartGuard",
+      "PublishDate": "2024-06-15",
+      "Des": "Supports cognitive function and memory enhancement."
+    }
+  ]
